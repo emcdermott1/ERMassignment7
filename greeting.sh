5e7d6fd993d0f35e4a186702da3a601ecc21fe99
vboxuser <vboxuser@CS100.myguest.virtualbox.org>
2023-11-30 14:52:01 -0500

    ERM1

---
+++ greeting.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+Hour=$(date +%H)
+
+WeekDay=$(date +%A)
+
+echo $Hour
+echo $WeekDay
+
+if [ $Hour -lt 12 ]; then
+	echo "Good Morning!"
+elif [ $Hour -lt 18 ]; then
+	echo "Good Afternoon"
+else
+	echo "Good Evening"
+fi
+
+
+if [ $WeekDay = "Friday" ]; then
+	echo "TGIF!"
+fi
